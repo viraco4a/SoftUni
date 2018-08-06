68ae1dcc613a39d231ae446004f883248f27c8f8
viraco4a <viraco4a@gmail.com>
2018-08-06 19:30:05 +0300

    PHP Lab part 1

---
+++ SoftwareTechnologies/PHP/PHPlab/Numbers1to20.php
@@ -0,0 +1,13 @@
+<ul>
+    <?php
+        for ($i = 1; $i <=20; $i++) {
+            if ($i % 2 != 0){
+                $color = "blue";
+            }
+            else{
+                $color = "green";
+            }
+            echo "<li style='color: $color'>$i</li>";
+        }
+    ?>
+</ul>
