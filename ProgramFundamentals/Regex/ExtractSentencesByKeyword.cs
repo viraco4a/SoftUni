7f886562dc5feeda798cf9821a3c885666461823
viraco4a <viraco4a@gmail.com>
2018-08-07 21:36:58 +0300

    test

---
+++ ProgramFundamentals/Regex/ExtractSentencesByKeyword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            string keyWord = Console.ReadLine();
+            string pattern = $@"(^|(?<=\s)){keyWord}($|(?=\s))";
+            var input = Console.ReadLine().Split(new char[] { '!', '.', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var sentence in input)
+            {
+                Match match = Regex.Match(sentence, pattern);
+                if (match.Success)
+                {
+                    Console.WriteLine(sentence.Trim());
+                }
+            }
+        }
+    }
+}
