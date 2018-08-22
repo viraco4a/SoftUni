e46622c4780160a0d323b8ac0dca870c03be67b5
viraco4a <viraco4a@gmail.com>
2018-08-22 07:25:12 +0300

    Java Adv Lab 1 part 2

--- JavaFundamentals/JavaAdvanced/IntroToJavaLab/EuroTrip.java
+++ JavaFundamentals/JavaAdvanced/IntroToJavaLab/EuroTrip.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class EuroTrip {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        
-
+        BigDecimal exchangeRate = new BigDecimal(4210500000000.0);
+        double kg = Double.parseDouble(scanner.nextLine());
+        BigDecimal priceInLevas = new BigDecimal(1.2 * kg);
+        BigDecimal result = priceInLevas.multiply(exchangeRate);
+        System.out.printf("%.2f marks", result);
     }
 }
