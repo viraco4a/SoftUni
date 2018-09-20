395bdacfb6f6d48086de3b09b8717c48c21e0963
viraco4a <viraco4a@gmail.com>
2018-09-20 17:38:57 +0300

    C# Adv Stacks and queues Lab part 1

---
+++ CSharpFundamentals/CSharpAdvanced/StacksAndQueues_Lab/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ReverseStrings
+{    static void Main()
+    {
+        string input = Console.ReadLine();
+        Stack<string> stack = new Stack<string>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            stack.Push(input[i].ToString());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            sb.Append(stack.Pop());
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
---
+++ CSharpFundamentals/CSharpAdvanced/StacksAndQueues_Lab/SimpleCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SimpleCalculator
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        Stack<string> stack = new Stack<string>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            try
+            {
+                int num = int.Parse(input[i]);
+                stack.Push(num)
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
