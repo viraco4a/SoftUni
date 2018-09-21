fb9c76bfe1d7f84840b1f49700f5e4dd257135bf
viraco4a <viraco4a@gmail.com>
2018-09-21 17:34:26 +0300

    C# Adv Stacks and Queues Ex final

---
+++ CSharpFundamentals/CSharpAdvanced/StacksAndQueues_Ex/PoisonousPlant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoisonousPlant
+{
+    class PoisonousPlant
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] plants = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Stack<int> indexesStack = new Stack<int>();
+            indexesStack.Push(0);
+
+            int[] days = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int maxDays = 0;
+
+                while (indexesStack.Count > 0 
+                    && int.Parse(plants[indexesStack.Peek()]) >= int.Parse(plants[i]))
+                {
+                    maxDays = Math.Max(maxDays, days[indexesStack.Pop()]);
+                }
+
+                if (indexesStack.Count > 0)
+                {
+                    days[i] = maxDays + 1;
+                }
+
+                indexesStack.Push(i);
+            }
+            Console.WriteLine(max(days));
+        }
+
+        private static int max(int[] days)
+        {
+            int max = int.MinValue;
+            foreach (int day in days)
+            {
+                if (day > max)
+                {
+                    max = day;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ CSharpFundamentals/CSharpAdvanced/StacksAndQueues_Ex/SimpleTextEditor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Stack<string> stackTmp = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int index = int.Parse(command[0]);
+                switch (index)
+                {
+                    case 1:
+                        stackTmp.Push(text.ToString());
+                        text.Append(command[1]);
+                        break;
+                    case 2:
+                        stackTmp.Push(text.ToString());
+                        int count = int.Parse(command[1]);
+                        int test = text.Length;
+                        text.Remove(Math.Max(0, text.Length - count), Math.Min(text.Length, count));
+                        break;
+                    case 3:
+                        int showIndex = int.Parse(command[1]);
+                        Console.WriteLine(text.ToString()[showIndex - 1]);
+                        break;
+                    case 4:
+                        text.Clear();
+                        text.Append(stackTmp.Pop());
+                        break;
+                    default:
+                        break;                            
+                }
+            }
+        }
+    }
+}
