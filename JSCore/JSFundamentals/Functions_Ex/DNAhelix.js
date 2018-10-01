75dbe0ebca828c2bea35aafe04bb5d98bafb7b6b
viraco4a <viraco4a@gmail.com>
2018-10-01 09:38:54 +0300

    JScore functions Ex final

--- JSCore/JSFundamentals/Functions_Ex/DNAhelix.js
+++ JSCore/JSFundamentals/Functions_Ex/DNAhelix.js
@@ -1,8 +1,52 @@
 function DNAhelix(n) {
-    for (let i = 0; i < n; i+=4) {
-              
+    let sequence = 'ATCGTTAGGG';
+    let first = (a, b) => {
+        console.log(`**${a}${b}**`);
+    }
+    let second = (a, b) => {
+        console.log(`*${a}--${b}*`);
+    }
+    let third = (a, b) => {
+        console.log(`${a}----${b}`);
+    }
+    let fourth = (a, b) => {
+        console.log(`*${a}--${b}*`);
+    }
+    for (let i = 0, j = 0; i < n; i++, j+=2) {
+        if (j === sequence.length){
+            j = 0;
+        }
+
+        first(sequence[j], sequence[j + 1]);
+        j+=2;
+        i++;
+        if (j === sequence.length){
+            j = 0;
+        }
+        if (i >= n){
+            break;
+        }
+        second(sequence[j], sequence[j + 1]);
+        j+=2;
+        i++;
+        if (j === sequence.length){
+            j = 0;
+        }
+        if (i >= n){
+            break;
+        }
+        third(sequence[j], sequence[j + 1]);
+        j+=2;
+        i++;
+        if (j === sequence.length){
+            j = 0;
+        }
+        if (i >= n){
+            break;
+        }
+        fourth(sequence[j], sequence[j + 1]);  
     }
 }
 
-DNAhelix(4);
+// DNAhelix(4);
 DNAhelix(10);
