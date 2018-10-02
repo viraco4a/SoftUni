4f0930da09277d28080aeea5691911f8594d18c0
viraco4a <viraco4a@gmail.com>
2018-10-02 12:13:36 +0300

    C# Adv sets and dictionaries Ex final

---
+++ CSharpFundamentals/CSharpAdvanced/SetsAndDictionaries_Ex/Ranking.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ranking
+{
+    class Ranking
+    {
+        private static Dictionary<string, SortedDictionary<string, int>> students;
+        private static Dictionary<string, string> contests;
+        private static string bestStudent;
+        private static int max;
+        static void Main(string[] args)
+        {
+            students = new Dictionary<string, SortedDictionary<string, int>>();
+
+            contests = new Dictionary<string, string>();
+
+            ReadContestData();
+
+            ReadSubmissionData();
+
+            PrintBest();
+
+            students
+                .OrderBy(o => o.Key)
+                .ToList()
+                .ForEach(s =>
+                {
+                    Console.WriteLine(s.Key);
+                    s.Value
+                    .OrderByDescending(o => o.Value)
+                    .ToList()
+                    .ForEach(k =>
+                    {
+                        Console.WriteLine($"#  {k.Key} -> {k.Value}");
+                    });
+                });
+        }
+
+        private static void PrintBest()
+        {
+            Console.WriteLine($"Best candidate is {bestStudent} with total {students[bestStudent].Sum(s => s.Value)} points.");
+            Console.WriteLine("Ranking:");
+        }
+
+        private static void ReadSubmissionData()
+        {
+            string input = Console.ReadLine();
+            while (input != "end of submissions")
+            {
+                var splitted = input.Split("=>");
+                string contest = splitted[0];
+                string pass = splitted[1];
+                string username = splitted[2];
+                int points = int.Parse(splitted[3]);
+                if (!contests.ContainsKey(contest))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                else if (contests[contest] != pass)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                if (!students.ContainsKey(username))
+                {
+                    students.Add(username, new SortedDictionary<string, int>());
+                }
+
+                if (!students[username].ContainsKey(contest))
+                {
+                    students[username].Add(contest, points);
+                }
+                else if (points > students[username][contest])
+                {
+                    students[username][contest] = points;
+                }
+
+                if (points > max)
+                {
+                    max = points;
+                    bestStudent = username;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void ReadContestData()
+        {
+            string input = Console.ReadLine();
+            while (input != "end of contests")
+            {
+                var splitted = input.Split(':');
+                if (!contests.ContainsKey(splitted[0]))
+                {
+                    contests.Add(splitted[0], splitted[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
--- CSharpFundamentals/CSharpAdvanced/SetsAndDictionaries_Ex/TheVlogger.cs
+++ CSharpFundamentals/CSharpAdvanced/SetsAndDictionaries_Ex/TheVlogger.cs
@@ -14,7 +14,38 @@ namespace TheVlogger
         {
             vloggers = new Dictionary<string, SortedSet<string>[]>();
             ReadInput();
+            PrintFirstLine();
+            PrintTopVlogger();
+            PrintVloggers();
+        }
 
+        private static void PrintVloggers()
+        {
+            int counter = 1;
+            vloggers
+                .OrderByDescending(a => a.Value[0].Count)
+                .ThenBy(x => x.Value[1].Count)
+                .ToList()
+                .ForEach(s =>
+                {
+                    Console.WriteLine($"{++counter}. {s.Key} : {s.Value[0].Count} followers, {s.Value[1].Count} following");
+                });
+        }
+
+        private static void PrintTopVlogger()
+        {
+            var top = vloggers[vloggerMax];
+            vloggers.Remove(vloggerMax);
+            Console.WriteLine($"1. {vloggerMax} : {top[0].Count} followers, {top[1].Count} following");
+            top[0].ToList().ForEach(s =>
+            {
+                Console.WriteLine($"*  {s}");
+            });
+        }
+
+        private static void PrintFirstLine()
+        {
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
         }
 
         private static void ReadInput()
@@ -28,7 +59,8 @@ namespace TheVlogger
                         .Split(" joined The V-Logger")[0];
                     if (!vloggers.ContainsKey(vlogger))
                     {
-                        vloggers.Add(vlogger, new SortedSet<string>[](); //TODO
+                        vloggers.Add(vlogger, new SortedSet<string>[] {
+                            new SortedSet<string>(), new SortedSet<string>()});
                     }
                 }
                 else
@@ -40,24 +72,26 @@ namespace TheVlogger
                         .Split(" followed ")[1];
 
                     if (!vloggers.ContainsKey(follower) ||
-                        !vloggers.ContainsKey(followee))
+                        !vloggers.ContainsKey(followee) ||
+                        follower == followee)
                     {
                         input = Console.ReadLine();
                         continue;
                     }
 
-                    vloggers[followee].Add(follower);
+                    vloggers[followee][0].Add(follower);
+                    vloggers[follower][1].Add(followee);
 
-                    if (vloggers[followee].Count == max)
+                    if (vloggers[followee][0].Count == max)
                     {
-                        if (vloggers[followee)
+                        if (vloggers[followee][1].Count < vloggers[vloggerMax][1].Count)
                         {
-                            //TODO
+                            vloggerMax = followee;
                         }
                     }
-                    if (vloggers[followee].Count > max)
+                    if (vloggers[followee][0].Count > max)
                     {
-                        max = vloggers[followee].Count;
+                        max = vloggers[followee][0].Count;
                         vloggerMax = followee;
                     }
                 }
