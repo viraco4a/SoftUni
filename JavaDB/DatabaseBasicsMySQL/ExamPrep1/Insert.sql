951fca29641b92fcd1adec2ae92772ee439d6ffb
viraco4a <viraco4a@gmail.com>
2018-10-09 07:02:07 +0300

    MySQL exam prep 1 part 1

---
+++ JavaDB/DatabaseBasicsMySQL/ExamPrep1/Insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO employees (first_name,
+						last_name, 
+                        gender,
+                        birthdate,
+                        department_id) 
+	VALUES 
+		('Marlo', 'O\'Malley', 'M', '1958-09-21', 1),
+        ('Niki', 'Stanaghan', 'F', '1969-11-26', 4), 
+        ('Ayrton', 'Senna', 'M', '1960-03-21', 9), 
+        ('Ronnie', 'Peterson', 'M', '1944-02-14', 9), 
+        ('Giovanna', 'Amati', 'F', '1959-07-20', 5);
+
+
+INSERT INTO reports (category_id,
+						status_id, 
+                        open_date,
+                        close_date,
+                        `description`,
+                        user_id,
+                        employee_id) 
+	VALUES -- TODO
+		('Marlo', 'O\'Malley', 'M', '1958-09-21', 1),
+        ('Niki', 'Stanaghan', 'F', '1969-11-26', 4), 
+        ('Ayrton', 'Senna', 'M', '1960-03-21', 9), 
+        ('Ronnie', 'Peterson', 'M', '1944-02-14', 9), 
+        ('Giovanna', 'Amati', 'F', '1959-07-20', 5);
