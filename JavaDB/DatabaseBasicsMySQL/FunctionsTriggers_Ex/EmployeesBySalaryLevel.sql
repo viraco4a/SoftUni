d30b243ff36319d92f8a947010b0e430e8fe43c9
viraco4a <viraco4a@gmail.com>
2018-10-15 13:29:49 +0300

    MySQL upd

--- JavaDB/DatabaseBasicsMySQL/FunctionsTriggers_Ex/EmployeesBySalaryLevel.sql
+++ JavaDB/DatabaseBasicsMySQL/FunctionsTriggers_Ex/EmployeesBySalaryLevel.sql
@@ -1,4 +1,17 @@
-DELIMITER $$
+CREATE FUNCTION ufn_get_salary_level (salary DECIMAL(17, 2))
+RETURNS VARCHAR(20)
+BEGIN
+	DECLARE salary_level VARCHAR(20);
+    SET salary_level = (
+		CASE
+			WHEN salary < 30000 THEN 'Low'
+            WHEN salary <= 50000 THEN 'Average'
+			ELSE 'High'
+		END
+    );
+	RETURN salary_level;
+END;
+
 CREATE PROCEDURE usp_get_employees_by_salary_level (salary_level VARCHAR(20))
 BEGIN
 	SELECT
@@ -8,10 +21,4 @@ BEGIN
 		employees AS e
 	WHERE ufn_get_salary_level(e.salary) = salary_level
     ORDER BY first_name DESC, last_name DESC;
-END $$
-DELIMITER ;
-
-CALL usp_get_employees_by_salary_level('High');
-
-DROP PROCEDURE usp_get_employees_by_salary_level;
-
+END
