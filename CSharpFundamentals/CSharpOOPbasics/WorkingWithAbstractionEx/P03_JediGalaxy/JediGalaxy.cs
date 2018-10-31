6feac3946dc63dcde708ab7c87289f3587ca54b7
viraco4a <viraco4a@gmail.com>
2018-10-31 07:11:03 +0200

    CSharp OOP basics Working with abstraction Ex part 1

---
+++ CSharpFundamentals/CSharpOOPbasics/WorkingWithAbstractionEx/P03_JediGalaxy/JediGalaxy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class JediGalaxy
+    {
+        private static int[,] matrix;
+        static void Main()
+        {
+            GetMatrix();
+
+            string command = Console.ReadLine();
+            long sum = 0;
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoStartCoordinates = ReadData(command);
+                int[] evilStartCoordinates = ReadData(Console.ReadLine());
+
+                ApplyEvilForce(evilStartCoordinates);
+
+                sum = CollectPower(sum, ivoStartCoordinates);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static long CollectPower(long sum, int[] ivoStartCoordinates)
+        {
+            int ivoStartRow = ivoStartCoordinates[0];
+            int ivoStartCol = ivoStartCoordinates[1];
+
+            while (ivoStartRow >= 0 && ivoStartCol < matrix.GetLength(1))
+            {
+                if (IsValid(ivoStartRow, ivoStartCol))
+                {
+                    sum += matrix[ivoStartRow, ivoStartCol];
+                }
+
+                ivoStartCol++;
+                ivoStartRow--;
+            }
+
+            return sum;
+        }
+
+        private static void ApplyEvilForce(int[] evilStartCoordinates)
+        {
+            int evilStartRow = evilStartCoordinates[0];
+            int evilStartCol = evilStartCoordinates[1];
+
+            while (evilStartRow >= 0 && evilStartCol >= 0)
+            {
+                if (IsValid(evilStartRow, evilStartCol))
+                {
+                    matrix[evilStartRow, evilStartCol] = 0;
+                }
+                evilStartRow--;
+                evilStartCol--;
+            }
+        }
+
+        private static void GetMatrix()
+        {
+            int[] dimestions = ReadData(Console.ReadLine());
+            int rows = dimestions[0];
+            int cols = dimestions[1];
+
+            matrix = new int[rows, cols];
+
+            int value = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = value++;
+                }
+            }
+        }
+
+        private static bool IsValid(int row, int col)
+        {
+            return 
+                row >= 0 &&
+                row < matrix.GetLength(0) &&
+                col >= 0 &&
+                col < matrix.GetLength(1);
+        }
+
+        private static int[] ReadData(string command)
+        {
+            return command
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ CSharpFundamentals/CSharpOOPbasics/WorkingWithAbstractionEx/P04_Hospital/Hospital.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Hospital
+{
+    public class Hospital
+    {
+        private static Dictionary<string, List<string>> doctors;
+        private static Dictionary<string, List<List<string>>> departments;
+        public static void Main()
+        {
+            doctors = new Dictionary<string, List<string>>();
+            departments = new Dictionary<string, List<List<string>>>();
+
+            string input = Console.ReadLine();
+            while (input != "Output")
+            {
+                string[] args = input.Split();
+                var departament = args[0];
+                var docFirstName = args[1];
+                var docLastName = args[2];
+                var patient = args[3];
+                var doctorName = docFirstName + docLastName;
+
+                FillData(departament, doctorName);
+
+                FillBeds(departament, patient, doctorName);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] args = input.Split();
+
+                Print(args);
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void Print(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Console.WriteLine(string.Join("\n",
+                    departments[args[0]]
+                    .Where(x => x.Count > 0)
+                    .SelectMany(x => x)));
+            }
+            else if (args.Length == 2 &&
+                int.TryParse(args[1],
+                out int room))
+            {
+                Console.WriteLine(string.Join("\n",
+                    departments[args[0]][room - 1]
+                    .OrderBy(x => x)));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("\n",
+                    doctors[args[0] + args[1]]
+                    .OrderBy(x => x)));
+            }
+        }
+
+        private static void FillBeds(string departament, string patient, string doctorName)
+        {
+            bool hasFreeBeds = departments[departament]
+                .SelectMany(x => x).Count() < 60;
+            if (hasFreeBeds)
+            {
+                int room = 0;
+                doctors[doctorName].Add(patient);
+                for (int i = 0; i < departments[departament].Count; i++)
+                {
+                    if (departments[departament][i].Count < 3)
+                    {
+                        room = i;
+                        break;
+                    }
+                }
+                departments[departament][room].Add(patient);
+            }
+        }
+
+        private static void FillData(string departament, string doctorName)
+        {
+            if (!doctors.ContainsKey(doctorName))
+            {
+                doctors[doctorName] = new List<string>();
+            }
+            if (!departments.ContainsKey(departament))
+            {
+                departments[departament] = new List<List<string>>();
+                for (int i = 0; i < 20; i++)
+                {
+                    departments[departament].Add(new List<string>());
+                }
+            }
+        }
+    }
+}
