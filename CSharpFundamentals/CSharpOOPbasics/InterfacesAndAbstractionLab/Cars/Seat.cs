6b772c9f240d0d9855fcf57116f7e6772eb0ac21
viraco4a <viraco4a@gmail.com>
2018-11-06 13:01:18 +0200

    CSharp OOP Basics Interfaces and Abstraction Lab Final

---
+++ CSharpFundamentals/CSharpOOPbasics/InterfacesAndAbstractionLab/Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
