64dd9156aaffd1fa5395742f56bbec65b1957a75
viraco4a <viraco4a@gmail.com>
2018-11-23 15:33:00 +0200

    JavaDB DB app Introduction Lab final

--- JavaDB/DatabaseAdvancedFrameworks/DBappsIntroductionLab/DiabloConnection/src/Main.java
+++ JavaDB/DatabaseAdvancedFrameworks/DBappsIntroductionLab/DiabloConnection/src/Main.java
@@ -1,18 +1,56 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        Properties props = new Properties();
-        props.setProperty("user", "root");
-        props.setProperty("password", "usaluboa");
-        Connection connection = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/diablo", props);
+    private static final String connectionString = "jdbc:mysql://localhost:3306/diablo";
+    private static final String USER = "root";
+    private static final String PASSWORD = "usaluboa";
 
-        PreparedStatement stmt =
-                connection.prepareStatement("SELECT * FROM users AS u WHERE u.user_name = ? ");
+    private static final String SQL =
+            "" +
+                    "SELECT " +
+                    "   CONCAT_WS(' ', u.first_name, u.last_name) AS full_name, " +
+                    "   COUNT(ug.id) AS games " +
+                    "FROM " +
+                    "   users AS u " +
+                    "       JOIN " +
+                    "   users_games AS ug ON ug.user_id = u.id " +
+                    "WHERE " +
+                    "   u.user_name = ?" +
+                    "GROUP BY u.id";
+
+    public static void main(String[] args) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", USER);
+        properties.setProperty("password", PASSWORD);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter player name: ");
+        String userName = scanner.nextLine();
+
+        Connection connection =
+                DriverManager.getConnection(connectionString, properties);
+        var preparedStatement = connection.prepareStatement(SQL);
+
+        preparedStatement.setString(1, userName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        boolean found = false;
+        while (resultSet.next()){
+            String fullName = resultSet.getString("full_name");
+            int games = resultSet.getInt("games");
+            System.out.printf("User: %s%n%s has played %d games",
+                    userName,
+                    fullName,
+                    games);
+            found = true;
+        }
+        if (!found){
+            System.out.println("No such user exists");
+        }
     }
 }
