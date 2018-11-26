fb948a11bdf2792ec543db5e66309f0ab5f8b7d3
viraco4a <viraco4a@gmail.com>
2018-11-26 18:07:42 +0200

    tmp

---
+++ JavaDB/DatabaseAdvancedFrameworks/com/entities/Employee.java
@@ -0,0 +1,31 @@
+package com.entities;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
