bdaec80bca64491fb191da8a9ac9349e04173e39
viraco4a <viraco4a@gmail.com>
2019-06-11 17:29:46 +0300

    Java OOP Unit Testing Lab part 1

--- JavaFundamentals/JavaOOP/UnitTesting_Lab/src/test/java/rpg_tests/AxeTests.java
+++ JavaFundamentals/JavaOOP/UnitTesting_Lab/src/test/java/rpg_tests/AxeTests.java
@@ -1,4 +1,28 @@
 package rpg_tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rpg_lab.Axe;
+import rpg_lab.Dummy;
+
 public class AxeTests {
+
+    @Test
+    public void weaponAttacksLosesDurability() {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        axe.attack(dummy);
+
+        Assert.assertEquals(9, axe.getDurabilityPoints());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void brokenWeaponCantAttack() {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        axe.attack(dummy);
+        axe.attack(dummy);
+    }
 }
