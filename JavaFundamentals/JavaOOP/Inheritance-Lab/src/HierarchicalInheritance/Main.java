9bfc2e5c4be61e407fc835f21618d2bf27be4cd4
viraco4a <viraco4a@gmail.com>
2019-07-29 16:02:31 +0300

    Java OOP Inheritance Lab part 3

--- JavaFundamentals/JavaOOP/Inheritance-Lab/src/HierarchicalInheritance/Main.java
+++ JavaFundamentals/JavaOOP/Inheritance-Lab/src/HierarchicalInheritance/Main.java
@@ -2,9 +2,12 @@ package HierarchicalInheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Puppy puppy = new Puppy();
-        puppy.eat();
-        puppy.bark();
-        puppy.weep();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
     }
 }
--- JavaFundamentals/JavaOOP/Inheritance-Lab/src/HierarchicalInheritance/Puppy.java
+++
@@ -1,7 +0,0 @@
-package HierarchicalInheritance;
-
-public class Puppy extends Dog {
-    public void weep() {
-        System.out.println("weeping...");
-    }
-}
