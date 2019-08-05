04f9bc7deb4f10f7e1c01cfe14f4f7b9a4deb7c3
viraco4a <viraco4a@gmail.com>
2019-08-05 16:00:53 +0300

    Java OOP Reflection Lab part 1

---
+++ JavaFundamentals/JavaOOP/Reflection-Lab/src/Main.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class reflection = Reflection.class;
+        System.out.println(reflection);
+        Class superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+        Class[] interfaces = reflection.getInterfaces();
+        for (Class anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
+        Reflection ref = (Reflection) reflection.getDeclaredConstructor().newInstance();
+        System.out.println(ref);
+
+    }
+}
