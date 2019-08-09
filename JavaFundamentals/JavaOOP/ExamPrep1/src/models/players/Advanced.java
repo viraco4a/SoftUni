261faf2adb7f1e3332a532b35689d37188bac4ab
viraco4a <viraco4a@gmail.com>
2019-08-09 10:21:55 +0300

    Java OOP Exam Prep 1 part 2

--- JavaFundamentals/JavaOOP/ExamPrep1/src/models/players/Advanced.java
+++ JavaFundamentals/JavaOOP/ExamPrep1/src/models/players/Advanced.java
@@ -5,7 +5,7 @@ import repositories.interfaces.CardRepository;
 public class Advanced extends BasePlayer {
     private static final int ADVANCED_DEFAULT_HEALTH_POINTS = 250;
 
-    protected Advanced(CardRepository cardRepository, String username) {
+    public Advanced(CardRepository cardRepository, String username) {
         super(cardRepository, username, ADVANCED_DEFAULT_HEALTH_POINTS);
     }
 }
--- JavaFundamentals/JavaOOP/ExamPrep1/src/models/players/Beginner.java
+++ JavaFundamentals/JavaOOP/ExamPrep1/src/models/players/Beginner.java
@@ -5,7 +5,7 @@ import repositories.interfaces.CardRepository;
 public class Beginner extends BasePlayer {
     private static final int BEGINNER_DEFAULT_HEALTH_POINTS = 50;
 
-    protected Beginner(CardRepository cardRepository, String username) {
+    public Beginner(CardRepository cardRepository, String username) {
         super(cardRepository, username, BEGINNER_DEFAULT_HEALTH_POINTS);
     }
 }
