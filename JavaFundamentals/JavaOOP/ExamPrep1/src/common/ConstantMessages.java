25a8bdee7b89ec5bc03a27ff893429d19ec308a4
viraco4a <viraco4a@gmail.com>
2019-08-09 10:08:36 +0300

    Java OOP Exam Prep 1 part 1

---
+++ JavaFundamentals/JavaOOP/ExamPrep1/src/common/ConstantMessages.java
@@ -0,0 +1,34 @@
+package common;
+
+public class ConstantMessages {
+
+    public static final String SUCCESSFULLY_ADDED_PLAYER =
+            "Successfully added player of type %s with username: %s";
+
+    public static final String SUCCESSFULLY_ADDED_CARD =
+            "Successfully added card of type %sCard with name: %s";
+
+    public static final String SUCCESSFULLY_ADDED_PLAYER_WITH_CARDS
+            = "Successfully added card: %s to user: %s";
+
+    public static final String FIGHT_INFO
+            = "Attack user health %s - Enemy user health %s";
+
+    public static final String PLAYER_REPORT_INFO
+            = "Username: %s - Health: %d - Cards %d";
+
+    public static final String CARD_REPORT_INFO
+            = "Card: %s - Damage: %d";
+
+    public static final String DEFAULT_REPORT_SEPARATOR
+            = "###";
+
+    public static final String PLAYER_USER_NULL_OR_EMPTY =
+            "Player's username cannot be null or an empty string.";
+
+    public static final String PLAYER_HEALTH_BELOW_ZERO =
+            "Player's health bonus cannot be less than zero.";
+
+    public static final String DAMAGE_POINTS_LESS_THAN_ZERO =
+            "Damage points cannot be less than zero.";
+}
---
+++ JavaFundamentals/JavaOOP/ExamPrep1/src/models/players/BasePlayer.java
@@ -0,0 +1,72 @@
+package models.players;
+
+import models.players.interfaces.Player;
+import repositories.interfaces.CardRepository;
+
+import static common.ConstantMessages.*;
+
+public abstract class BasePlayer implements Player {
+    private static final int HEALTH_POINTS_MIN = 0;
+
+    private String username;
+    private int health;
+    private CardRepository cardRepository;
+    private boolean isDead;
+
+    public BasePlayer(String username, int health, CardRepository cardRepository) {
+        this.setUsername(username);
+        this.setHealth(health);
+        this.setCardRepository(cardRepository);
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public CardRepository getCardRepository() {
+        return this.cardRepository;
+    }
+
+    @Override
+    public void setHealth(int healthPoints) {
+        if (healthPoints < HEALTH_POINTS_MIN) {
+            throw new IllegalArgumentException(PLAYER_HEALTH_BELOW_ZERO);
+        }
+        this.health = healthPoints;
+    }
+
+    private void setUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new IllegalArgumentException(PLAYER_USER_NULL_OR_EMPTY);
+        }
+        this.username = username;
+    }
+
+    private void setCardRepository(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    private void setDead(boolean dead) {
+        isDead = dead;
+    }
+
+    @Override
+    public boolean isDead() {
+        return this.isDead;
+    }
+
+    @Override
+    public void takeDamage(int damagePoints) {
+        if (damagePoints < HEALTH_POINTS_MIN) {
+            throw new IllegalArgumentException(DAMAGE_POINTS_LESS_THAN_ZERO);
+        }
+        this.setHealth(this.getHealth() - damagePoints);
+    }
+}
