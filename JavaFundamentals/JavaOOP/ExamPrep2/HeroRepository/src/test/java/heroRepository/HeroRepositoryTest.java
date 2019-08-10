b194bb467adea5d8ea06e6b5245c314b3fcdfca3
Kaloyan <viraco4a@gmail.com>
2019-08-10 19:32:30 +0300

    Java OOP exam prep 2 unit tests 60/100

--- JavaFundamentals/JavaOOP/ExamPrep2/HeroRepository/src/test/java/heroRepository/HeroRepositoryTest.java
+++ JavaFundamentals/JavaOOP/ExamPrep2/HeroRepository/src/test/java/heroRepository/HeroRepositoryTest.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HeroRepositoryTest {
+    private static final String PESHO_TO_STRING = String.format("Hero: Pesho – 5%n" +
+            "  *  Strength: 10%n" +
+            "  *  Agility: 10%n" +
+            "  *  Intelligence: 10%n");
     private HeroRepository heroRepository;
     private Hero hero;
     private Item item;
@@ -79,5 +83,30 @@ public class HeroRepositoryTest {
         Item item2 = new Item(9, 9, 9);
         this.heroRepository.add(new Hero("Gosho", 5, item2));
         Assert.assertEquals(this.hero, this.heroRepository.getHeroWithHighestStrength());
+    }
+
+    @Test
+    public void shouldReturnCorrectHeroInformationAsString() {
+        this.heroRepository.add(this.hero);
+        String actual = this.heroRepository.toString();
+        Assert.assertEquals(PESHO_TO_STRING, actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowWhenNoHigherWithHighestStrengthFound() {
+        Hero hero2 = new Hero("Gosho", 5, new Item(-1, -1, -1));
+        this.heroRepository.getHeroWithHighestStrength();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowWhenNoHigherWithHighestAgilityound() {
+        Hero hero2 = new Hero("Gosho", 5, new Item(-1, -1, -1));
+        this.heroRepository.getHeroWithHighestAgility();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowWhenNoHigherWithHighestIntelligenceFound() {
+        Hero hero2 = new Hero("Gosho", 5, new Item(-1, -1, -1));
+        this.heroRepository.getHeroWithHighestIntelligence();
     }
 }
