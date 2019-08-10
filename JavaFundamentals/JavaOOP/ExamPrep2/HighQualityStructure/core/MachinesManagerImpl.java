00748190128788df214bad80121cb1d4e8ea9e81
Kaloyan <viraco4a@gmail.com>
2019-08-10 12:35:42 +0300

    Java OOP exam prep 2 task 1 50/50, task 2 105/150

--- JavaFundamentals/JavaOOP/ExamPrep2/HighQualityStructure/core/MachinesManagerImpl.java
+++ JavaFundamentals/JavaOOP/ExamPrep2/HighQualityStructure/core/MachinesManagerImpl.java
@@ -3,8 +3,10 @@ package core;
 import core.interfaces.MachineFactory;
 import core.interfaces.MachinesManager;
 import core.interfaces.PilotFactory;
+import entities.interfaces.Fighter;
 import entities.interfaces.Machine;
 import entities.interfaces.Pilot;
+import entities.interfaces.Tank;
 
 import java.util.Map;
 
@@ -30,18 +32,34 @@ public class MachinesManagerImpl implements MachinesManager {
     @Override
     public String hirePilot(String name) {
         Pilot pilot = this.pilotFactory.createPilot(name);
-        this.pilots.put(name, pilot);
-        return String.format(pilotHired,name);
+        if (this.pilots.containsKey(name)) {
+            return String.format(pilotExists, name);
+        } else {
+            this.pilots.put(name, pilot);
+            return String.format(pilotHired, name);
+        }
     }
 
     @Override
     public String manufactureTank(String name, double attackPoints, double defensePoints) {
-        return null;
+        Tank tank = this.machineFactory.createTank(name, attackPoints, defensePoints);
+        if (this.machines.containsKey(name)) {
+            return String.format(machineExists, name);
+        } else {
+            this.machines.put(name, tank);
+            return String.format(tankManufactured, name, attackPoints, defensePoints);
+        }
     }
 
     @Override
     public String manufactureFighter(String name, double attackPoints, double defensePoints) {
-        return null;
+        Fighter fighter = this.machineFactory.createFighter(name, attackPoints, defensePoints);
+        if (this.machines.containsKey(name)) {
+            return String.format(machineExists, name);
+        } else {
+            this.machines.put(name, fighter);
+            return String.format(fighterManufactured, name, attackPoints, defensePoints);
+        }
     }
 
     @Override
