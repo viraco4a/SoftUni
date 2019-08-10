a039f2bf6bcdb0b8588feb92f87b969d87f20413
Kaloyan <viraco4a@gmail.com>
2019-08-10 09:08:53 +0300

    Java OOP Exam Prep 1 part 9

--- JavaFundamentals/JavaOOP/ExamPrep1_unit/src/test/java/parkingSystem/SoftParkTest.java
+++ JavaFundamentals/JavaOOP/ExamPrep1_unit/src/test/java/parkingSystem/SoftParkTest.java
@@ -1,6 +1,49 @@
 package parkingSystem;
 
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SoftParkTest {
-  //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS SoftPark
+    private SoftPark park;
+    private Car car;
+
+    @Before
+    public void initialize() {
+        this.park = new SoftPark();
+        this.car = new Car("dd", "1234");
+
+    }
+
+    @Test
+    public void testValidImplementationConstructor() {
+        int actualParkCount = this.park.getParking().size();
+
+        Assert.assertEquals(12, actualParkCount);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testModifyUnmodifiableParkingLot() {
+        this.park.getParking().remove("A1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParkCarThrowsExceptionWhenCarSpotNotExist() {
+        this.park.parkCar("D1", this.car);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParkCarThrowsExceptionWhenCarSpotTaken() {
+        this.park.parkCar("A1", this.car);
+        Car car2 = new Car("aa", "4321");
+        this.park.parkCar("A1", car2);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testParkCarThrowsExceptionWhenCarAlreadyParked() {
+        this.park.parkCar("A1", this.car);
+        this.park.parkCar("A2", this.car);
+    }
+
 }
