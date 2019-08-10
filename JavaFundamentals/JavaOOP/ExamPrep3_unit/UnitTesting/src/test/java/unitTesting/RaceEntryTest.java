08bde8984f5a92cc8294ec69a6c1ba48fb190228
Kaloyan <viraco4a@gmail.com>
2019-08-10 19:50:15 +0300

    Java OOP exam prep 3 unit test 40/100

---
+++ JavaFundamentals/JavaOOP/ExamPrep3_unit/UnitTesting/src/test/java/unitTesting/RaceEntryTest.java
@@ -0,0 +1,27 @@
+package unitTesting;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RaceEntryTest {
+    private RaceEntry raceEntry;
+    private static final UnitMotorcycle UNIT_MOTORCYCLE = new UnitMotorcycle("M1", 10, 10);
+    private static final UnitRider UNIT_RIDER = new UnitRider("R1", UNIT_MOTORCYCLE);
+
+    @Before
+    public void init() {
+        this.raceEntry = new RaceEntry();
+    }
+
+    @Test
+    public void ConstructorTest() {
+        this.raceEntry.addRider(UNIT_RIDER);
+        Assert.assertEquals(1, this.raceEntry.getRiders().size());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void AddNullRider() {
+        this.raceEntry.addRider(null);
+    }
+}
